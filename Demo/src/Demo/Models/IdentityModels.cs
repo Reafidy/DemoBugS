f9d36de51eaaec44cdff73b95aed62c36d851c45
Reafidy <reafidy@hotmail.com>
2015-07-04 22:13:53 +1200

    Basic Project

---
+++ Demo/src/Demo/Models/IdentityModels.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Framework.OptionsModel;
+
+namespace Demo.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+
+        public virtual UserProfile UserProfile { get; set; }
+    }
+
+   
+
+
+    public class UserProfile
+    {
+
+     
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //public virtual IdentityUser IdentityUser { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        private static bool _created;
+
+        public DbSet<UserProfile> UserProfile { get; set; }
+
+        public ApplicationDbContext()
+        {
+            // Create the database and schema if it doesn't exist
+            if (!_created)
+            {
+                Database.AsRelational().ApplyMigrations();
+                _created = true;
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
